25b759475282e01b96c76873563b9df9bbbc16bf
retsrif <retsrif@gmail.com>
2012-12-24 02:52:47 +0300

    derp

--- src/main/java/com/volumetricpixels/testmodule/TestModule.java
+++ src/main/java/com/volumetricpixels/testmodule/TestModule.java
@@ -9,7 +9,6 @@ import org.spout.api.plugin.CommonPlugin;
 import org.spout.vanilla.material.InitializableMaterial;
 import org.spout.vanilla.material.VanillaBlockMaterial;
 import org.spout.vanilla.material.VanillaMaterial;
-import org.spout.vanilla.material.VanillaMaterials;
 
 public class TestModule extends CommonPlugin {
 	public static final WindmillBlock w = new WindmillBlock("Windmill", 512);
@@ -24,7 +23,7 @@ public class TestModule extends CommonPlugin {
 	}
 	
 	public static void initialize() {
-		for (Field field : VanillaMaterials.class.getFields()) {
+		for (Field field : TestModule.class.getFields()) {
 			try {
 				if (field == null || ((field.getModifiers() & (Modifier.STATIC | Modifier.PUBLIC)) != (Modifier.STATIC | Modifier.PUBLIC)) || !VanillaMaterial.class.isAssignableFrom(field.getType())) {
 					continue;
