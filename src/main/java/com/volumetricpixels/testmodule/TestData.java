a48d214f8e61724b1ed475e7f2e322c7cf844513
James Hutchison <th3hutch@yahoo.co.uk>
2012-12-27 04:18:17 +0000

    Refractored

---
+++ src/main/java/com/volumetricpixels/testmodule/TestData.java
@@ -0,0 +1,10 @@
+package com.volumetricpixels.testmodule;
+
+public class TestData {
+
+    private TestData() {
+    }
+    
+    public static final WindmillBlock WINDMILL_BLOCK = new WindmillBlock("Windmill", 150);
+    
+}
