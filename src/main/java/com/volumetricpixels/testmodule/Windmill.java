67bdd96e315c96cd313fd9dfa6629ac1b0ea2c6e
James Hutchison <th3hutch@yahoo.co.uk>
2012-12-27 04:18:06 +0000

    Updated for API change

--- src/main/java/com/volumetricpixels/testmodule/Windmill.java
+++ src/main/java/com/volumetricpixels/testmodule/Windmill.java
@@ -1,17 +1,13 @@
 package com.volumetricpixels.testmodule;
 
 import com.volumetricpixels.mcsquared.api.energy.EnergyGenerator;
-import org.spout.api.entity.Player;
-import org.spout.api.inventory.Inventory;
-import org.spout.vanilla.component.inventory.WindowHolder;
-import org.spout.vanilla.inventory.window.block.CraftingTableWindow;
 
 public class Windmill extends EnergyGenerator {
 	
     private int energy;
 
     public Windmill() {
-        final int height = this.getBlock().getPosition().getBlockY();
+        final int height = this.getPosition().getBlockY();
         energy = height - (height % 10);
     }
 	
@@ -19,18 +15,9 @@ public class Windmill extends EnergyGenerator {
 	public void onTick(float dt) {
 		onEnergyGenerate(energy);
 	}
-	
-	public Inventory getInventory() {
-		return null;
-	}
-
-	public void open(Player player) {
-		player.get(WindowHolder.class).openWindow(new CraftingTableWindow(player));
-		player.sendMessage("Energy: " + energy);
-	}
 
 	@Override
 	public void onEnergyGenerate(double energy_generated) {
         //TODO: Split energy to each connected block
-	}    
+	}
 }
