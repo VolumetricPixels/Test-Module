53819ffbfd41984e431ede4799456a5f71ac595d
retsrif <retsrif@gmail.com>
2012-12-27 03:08:16 +0000

    Trying to add block to game

---
+++ src/main/java/com/volumetricpixels/testmodule/TMExecutor.java
@@ -0,0 +1,29 @@
+package com.volumetricpixels.testmodule;
+
+import org.spout.api.command.Command;
+import org.spout.api.command.CommandContext;
+import org.spout.api.command.CommandExecutor;
+import org.spout.api.command.CommandSource;
+import org.spout.api.component.impl.HitBlockComponent;
+import org.spout.api.entity.Player;
+import org.spout.api.exception.CommandException;
+import org.spout.api.geo.cuboid.Block;
+
+public class TMExecutor implements CommandExecutor {
+
+	public void processCommand(CommandSource source, Command command, CommandContext args) throws CommandException {
+		String name = command.getPreferredName();
+		Player p = (Player)source;
+		
+		if(name.equalsIgnoreCase("+windmill")) {
+			p.sendMessage("hi");
+			HitBlockComponent hit = p.get(HitBlockComponent.class);
+			if(hit!=null) {
+				p.sendMessage("hi");
+				Block b = hit.getTargetBlock();
+				b.setMaterial(WindmillData.WINDMILL_BLOCK);
+			}
+		}
+	}
+
+}
