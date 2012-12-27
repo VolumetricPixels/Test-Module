817637d69f065b699a3b29986abd031b4f320b37
James Hutchison <th3hutch@yahoo.co.uk>
2012-12-27 15:54:59 +0000

    Now implements TestBlockMaterial

--- src/main/java/com/volumetricpixels/testmodule/materials/batteryblock/BatteryBlock.java
+++ src/main/java/com/volumetricpixels/testmodule/materials/batteryblock/BatteryBlock.java
@@ -1,10 +1,22 @@
 package com.volumetricpixels.testmodule.materials.batteryblock;
 
+import com.volumetricpixels.testmodule.TestMaterials;
+import com.volumetricpixels.testmodule.materials.TestBlockMaterial;
+import org.spout.api.Spout;
+import org.spout.api.inventory.recipe.RecipeBuilder;
+import org.spout.vanilla.material.VanillaMaterials;
 import org.spout.vanilla.material.block.component.ComponentMaterial;
 
-public class BatteryBlock extends ComponentMaterial {
+public class BatteryBlock extends ComponentMaterial implements TestBlockMaterial {
 
 	public BatteryBlock(String name, int id) {
 		super(name, id, Battery.class, "model://Vanilla/materials/block/solid/oakplanks/oakplanks.spm");
 	}
+
+    public void initRecipe() {
+        RecipeBuilder builder = new RecipeBuilder();
+        builder.setResult(TestMaterials.BATTERY_BLOCK, 1);
+        builder.addIngredient(VanillaMaterials.BEDROCK);
+        Spout.getEngine().getRecipeManager().register(builder.buildShapelessRecipe());
+    }
 }
