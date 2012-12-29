346b864cb4301a31029d48114b20402a81a00277
User <User@User-TOSH.home>
2012-12-29 22:05:43 +0000

    Changed TestBlockMaterial into abstract class

--- src/main/java/com/volumetricpixels/testmodule/materials/batteryblock/BatteryBlock.java
+++ src/main/java/com/volumetricpixels/testmodule/materials/batteryblock/BatteryBlock.java
@@ -1,13 +1,12 @@
 package com.volumetricpixels.testmodule.materials.batteryblock;
 
 import com.volumetricpixels.testmodule.TestMaterials;
-import com.volumetricpixels.testmodule.materials.ComponentMaterial;
 import com.volumetricpixels.testmodule.materials.TestBlockMaterial;
 import org.spout.api.Spout;
 import org.spout.api.inventory.recipe.RecipeBuilder;
 import org.spout.vanilla.material.VanillaMaterials;
 
-public class BatteryBlock extends ComponentMaterial implements TestBlockMaterial {
+public class BatteryBlock extends TestBlockMaterial {
 
 	public BatteryBlock(String name) {
 		super(name, Battery.class, "model://Test-Module/materials/block/solid/baterry/battery.spm");
--- src/main/java/com/volumetricpixels/testmodule/materials/windmill/WindmillBlock.java
+++ src/main/java/com/volumetricpixels/testmodule/materials/windmill/WindmillBlock.java
@@ -1,13 +1,12 @@
 package com.volumetricpixels.testmodule.materials.windmill;
 
 import com.volumetricpixels.testmodule.TestMaterials;
-import com.volumetricpixels.testmodule.materials.ComponentMaterial;
 import com.volumetricpixels.testmodule.materials.TestBlockMaterial;
 import org.spout.api.Spout;
 import org.spout.api.inventory.recipe.RecipeBuilder;
 import org.spout.vanilla.material.VanillaMaterials;
 
-public class WindmillBlock extends ComponentMaterial implements TestBlockMaterial {
+public class WindmillBlock extends TestBlockMaterial {
 
 	public WindmillBlock(String name) {
 		super(name, Windmill.class, "model://Vanilla/materials/block/solid/furnace/furnace.spm");
