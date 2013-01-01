b0f2862a625fe6f1b0e6f2cff5f012c2a8d02f13
Jack Huey <kitskub@gmail.com>
2013-01-01 17:20:15 -0600

    Update wire

--- src/main/java/com/volumetricpixels/testmodule/materials/wire/Wire.java
+++ src/main/java/com/volumetricpixels/testmodule/materials/wire/Wire.java
@@ -1,51 +1,19 @@
 package com.volumetricpixels.testmodule.materials.wire;
 
 import com.volumetricpixels.mcsquared.api.energy.Energy;
-import com.volumetricpixels.mcsquared.api.energy.EnergyNode;
+import com.volumetricpixels.mcsquared.api.energy.EnergyCarrier;
 import com.volumetricpixels.mcsquared.api.energy.EnergyReceiver;
 import com.volumetricpixels.mcsquared.api.energy.EnergySource;
 import com.volumetricpixels.mcsquared.api.energy.MaxEnergySource;
-import com.volumetricpixels.mcsquared.api.energy.impl.EnergyNodeImpl;
-import com.volumetricpixels.mcsquared.api.utils.EnergyUtils;
-import java.util.HashSet;
-import java.util.Set;
-import org.spout.vanilla.material.VanillaMaterials;
 
-public class Wire extends EnergyNodeImpl implements EnergyReceiver, EnergySource, MaxEnergySource {
-
-    private Energy maxEnergy;
-    private final Set<EnergyReceiver> receivers = new HashSet<EnergyReceiver>();
+public class Wire extends EnergyCarrier implements EnergyReceiver, EnergySource, MaxEnergySource {
     
     public Wire() {
-        maxEnergy = new Energy(100);
+        this.maxEnergyTransfer = new Energy(100);
     }
 
     @Override
-    public Energy onReceive(EnergySource source, Set<EnergyNode> visited, Energy energy) {
-        if (maxEnergy.compareTo(energy) > 0) {
-            this.getPosition().getBlock().setMaterial(VanillaMaterials.AIR);
-            //Play explosion sound
-            return energy;
-        }
-        visited.add(this);
-        return EnergyUtils.safeSplit(source, energy, visited, receivers);
-    }
-
-    @Override
-    public void addReceiver(EnergyReceiver destination) {
-        receivers.add(destination);
-    }
-
     public Energy getMaxEnergy() {
-        return maxEnergy;
-    }
-
-    public void setMaxEnergy(Energy maxEnergy) {
-        this.maxEnergy = maxEnergy;
-    }
-    
-    @Override
-    public void removeReceiver(EnergyReceiver receiver) {
-        receivers.remove(receiver);
+        return maxEnergyTransfer;
     }
 }
