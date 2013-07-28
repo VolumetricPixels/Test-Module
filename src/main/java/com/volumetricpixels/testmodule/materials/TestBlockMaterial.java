29787d5730613a12afc6b7cd629ae62b194c6d6c
thehutch <th3hutch@yahoo.co.uk>
2013-07-28 23:26:24 +0100

    Updated for API changes Signed-off-by:thehutch <th3hutch@yahoo.co.uk>

--- src/main/java/com/volumetricpixels/testmodule/materials/TestBlockMaterial.java
+++ src/main/java/com/volumetricpixels/testmodule/materials/TestBlockMaterial.java
@@ -1,32 +1,23 @@
 package com.volumetricpixels.testmodule.materials;
 
+import java.util.Arrays;
 import org.spout.api.Platform;
 import org.spout.api.Spout;
 import org.spout.api.component.block.BlockComponent;
-import org.spout.api.material.BlockMaterial;
-import org.spout.api.material.Material;
+import org.spout.api.material.basic.Solid;
 import org.spout.vanilla.render.VanillaEffects;
 
-public abstract class TestBlockMaterial extends BlockMaterial implements Initializable {
+public abstract class TestBlockMaterial extends Solid implements Initializable {
 
-	public TestBlockMaterial(String name, String model, Class<? extends BlockComponent>... component) {
-		super((short) 0, name, model, component);
+	public TestBlockMaterial(String name, String model, Class<? extends BlockComponent>... components) {
+		super(name, model);
 		if (Spout.getEngine().getPlatform() == Platform.CLIENT) {
 			if (!getModel().getRenderMaterial().getRenderEffects().contains(VanillaEffects.SKY_TIME)) {
 				getModel().getRenderMaterial().addRenderEffect(VanillaEffects.SKY_TIME);
 				getModel().getRenderMaterial().addBufferEffect(VanillaEffects.LIGHTING);
 			}
 		}
-	}
-
-	public TestBlockMaterial(String name, int data, Material parent, String model, Class<? extends BlockComponent> component) {
-		super(name, data, parent, model, component);
-		if (Spout.getEngine().getPlatform() == Platform.CLIENT) {
-			if (!getModel().getRenderMaterial().getRenderEffects().contains(VanillaEffects.SKY_TIME)) {
-				getModel().getRenderMaterial().addRenderEffect(VanillaEffects.SKY_TIME);
-				getModel().getRenderMaterial().addBufferEffect(VanillaEffects.LIGHTING);
-			}
-		}
+		getComponents().addAll(Arrays.asList(components));
 	}
 
 	public abstract void initialise();
